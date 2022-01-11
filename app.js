ad165889242b02322b6c49bfe197ee52e9c5b21e
Scott Lease <scottie_lease@hotmail.com>
2022-01-10 18:23:45 -0800

    testing passes

---
+++ app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const express = require('express');
+const req = require('express/lib/request');
+
+// express returns a singleton pattern(returns an object that can be modified)
+const app = express();
+
+const messages = [];
+class Message {
+  constructor(text, author) {
+    this.text = text;
+    this.author = author;
+  }
+}
+
+// needs 2 things: a route(string) and a Callback function - tells the route to do, using 2 params: req and res
+app.get('/message', (req, res) => {
+// create a message and send it back
+  console.log('Someone sent a request! ' + req.method);
+  res.send(messages);
+}); // this method functio modifies our singleton
+
+function createMessage(req, res, next) {
+  const messageText = req.query.text;
+  const authorName = req.query.author;
+  const message = new Message(messageText, authorName);
+  // we modify
+  req.message = message;
+  next();
+}
+
+function saveMessage(req, res, next) {
+  let message = req.message;
+  messages.push(message);
+  next();
+}
+
+// POST http://localhost:300/message?text=someString&author=Jacob
+app.post('/message', createMessage, saveMessage, (req, res, next) => {
+// create a message and send it back?
+//   const messageText = req.query.text;
+//   const authorName = req.query.author;
+
+  //   next('an error has occured'); //error handling for server
+  //   const message = new Message(messageText, authorName);
+  //   MessageChannel.push(message);
+  res.send(messages);
+});
+
+app.use(function (err, req, res, next) {
+  res.send('Error Handler hit!');
+});
+
+
+// only runs when no other functio can from handlers above
+app.use(function (req, res) {
+  res.status(404).send('Nothing Found');
+});
+
+// module.exports = app;
+
+module.exports = {
+  start: function (port) {
+    app.listen(port, () => {
+      console.log('App is running on: ' + port);
+    });
+  },
+  app,
+};
